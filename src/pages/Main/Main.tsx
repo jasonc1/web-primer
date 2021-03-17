0eaecb8c8d51a0b4ec094c345f0dd952f6d8df54
Jason Chen <jasonc1@andrew.cmu.edu>
2021-03-16 23:51:11 -0700

    added mock main app component

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.style.scss';
+
+export const Main = () => {
+  return (
+    <div className="row container main">
+      <div className="cols twelve">Main app container</div>
+    </div>
+  );
+};
+
+export default Main;
