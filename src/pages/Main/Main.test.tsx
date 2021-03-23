ab612ad9d7d92ffd9a8973170c1faf335b036a68
Jason Chen <jasonc1@andrew.cmu.edu>
2021-03-23 10:59:35 -0700

    updated main.tsx test text

--- src/pages/Main/Main.test.tsx
+++ src/pages/Main/Main.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Main from './Main';
 
-test('renders learn react link', () => {
+test('renders main app text', () => {
   render(<Main />);
   const TextElement = screen.getByText('Main app container');
   expect(TextElement).toBeInTheDocument();
